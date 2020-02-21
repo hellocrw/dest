de35dc1afc01e23073ab407e5131f475bb9d2b17
= <2388092655@qq.com>
2020-02-22 02:44:23 +0800

    初始化测试

---
+++ src/main/java/crw/study/dest/controller/TestController.java
@@ -0,0 +1,10 @@
+package crw.study.dest.controller;
+
+/**
+ * @Description Description
+ * @Author crw
+ * @Date Created in 2020/2/22 0022
+ * @Time 2:29
+ */
+public class TestController {
+}
---
+++ src/test/java/crw/study/dest/DestApplicationTests.java
@@ -0,0 +1,13 @@
+package crw.study.dest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
